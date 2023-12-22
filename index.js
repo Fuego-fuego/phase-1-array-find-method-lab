73a6a2253a71e0e2d89f6a88dc2ca1de041f0137
nathaniEL mujesia <nathanielmujesia@gmail.com>
2023-12-22 15:12:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -3,19 +3,16 @@
 /*  Function superbowlWin , 
     To find a year a team won based on supplied records*/
 function superbowlWin(record) {
+    
     // Find object containing a year won using find() method
-    const winYear = record.find(
-        element => {
-
-            if (element.result === "W") {
-
-                return element;
-            }
-
+    const winYear = record.find(element => {
+        if (element.result === 'W') {
+            return element;
         }
-    );
+    });
+
     // ternary operator used to assign a result based on find method
     let result = winYear ? winYear['year'] : undefined;
 
     return result;
-};
+}
