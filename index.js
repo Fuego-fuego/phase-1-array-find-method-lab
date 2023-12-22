b92ab1fa60e708c0209ba70daa0b6393a606fb4c
nathaniEL mujesia <nathanielmujesia@gmail.com>
2023-12-22 15:01:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(record) {
+    
+    const winYear = record.find(
+        element => {
+
+            if (element.result === "W") {
+
+                return element;
+            }
+            
+        }
+    );
+    let result = winYear?winYear['year']:undefined;
+    
+    return result;
+};
